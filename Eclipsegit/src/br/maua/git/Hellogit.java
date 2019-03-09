98d381eb7c9c158aa772d1a2c2a8c4db0509d9ab
Fábio Kawasaki <binhu_kawa@hotmail.com>
2019-03-09 12:27:19 -0300

    Update Hellogit.java

--- Eclipsegit/src/br/maua/git/Hellogit.java
+++ Eclipsegit/src/br/maua/git/Hellogit.java
@@ -3,7 +3,9 @@ package br.maua.git;
 public class Hellogit {
 
 	public static void main(String[] args) {
-		System.out.println(" Hello git...");
+		System.out.println("Hello git...");
+		
+		System.out.println("Hello GIT - ALTERADO NO GITHUB..."
 
 	}
 
